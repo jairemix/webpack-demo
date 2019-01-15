a4ef46c570d0dfadb668f75403bd02450ceb0ce4
Jeremy Li <mail.jeremy.li@gmail.com>
2019-01-15 16:24:48 +1100

    using environment variables

--- src/index.js
+++ src/index.js
@@ -7,8 +7,24 @@ function component() {
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = join(['It', 'works!'], ' ');
-
   return element;
 }
 
 document.body.appendChild(component());
+
+console.log('WEBPACK_ENV', WEBPACK_ENV);
+
+if (WEBPACK_ENV.DEBUG) {
+  document.body.appendChild((() => {
+    const element = document.createElement('p');
+    element.innerHTML = 'Debugging enabled';
+    return element;
+  })());
+}
+
+document.body.appendChild((() => {
+  const element = document.createElement('p');
+  element.innerHTML = `Mode is ${WEBPACK_ENV.MODE}`;
+  return element;
+})());
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,41 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+module.exports = (env) => {
+  const isProd = env && env.production;
+  const mode = isProd ? 'production' : 'development';
+  console.log(`🗻 environment: ${mode}\n`);
+  return {
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9002,
+    },
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    mode,
+    optimization: {
+      minimize: false
+    },
+    plugins: [
+      new HTMLWebpackPlugin({ template: './src/index.html' }),
+      new webpack.DefinePlugin({
+        WEBPACK_ENV: {
+          DEBUG: JSON.stringify(!isProd),
+          MODE: JSON.stringify(mode),
+        },
+      }),
     ],
-  },
-  plugins: [
-    new HTMLWebpackPlugin({ template: './src/index.html' }),
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9002,
-  },
+  };
 };
